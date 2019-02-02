9bd7da33231c1d1efc716f90af574f0340af6f6d
liudan6 <liudan6@staff.sina.com.cn>
2019-02-02 18:31:44 +0800

    修复通用链接拼接bug

--- initData/template/default/src/scripts/function/sharePage.js
+++ initData/template/default/src/scripts/function/sharePage.js
@@ -130,7 +130,13 @@ define(['jQuery', 'doT', 'utils', 'ajaxUtil', 'wapCallNative', 'swiper', 'lazylo
       var link = $this.attr('doc-url');
       if (link && link !== '') {
         if (link == 'currentPage') {
-          location.href = window.PAGEDATA.universalLink + '&number=' + window.PAGEDATA.number;
+          var gotoLink = window.PAGEDATA.universalLink;
+          if (gotoLink.indexOf('?') > 0) {
+            gotoLink += '&number=' + window.PAGEDATA.number;
+          } else {
+            gotoLink += '?number=' + window.PAGEDATA.number;
+          }
+          location.href = gotoLink;
         } else if (link == 'appIndex') { // 呼起APP，打开APP首页
           location.href = window.PAGEDATA.appDownload + '?number=' + window.PAGEDATA.number;
         } else { // 根据当前指定url获取通用链接，呼起APP指定界面
@@ -254,7 +260,6 @@ define(['jQuery', 'doT', 'utils', 'ajaxUtil', 'wapCallNative', 'swiper', 'lazylo
                 el: '.pagination1',
               },
             })
-
           }
         }
       });
@@ -272,29 +277,22 @@ define(['jQuery', 'doT', 'utils', 'ajaxUtil', 'wapCallNative', 'swiper', 'lazylo
           var images = msg.data.image;
           $('#sharePicPoster').attr('src', images);
           var data = {
-
             playerRatio: (9/16),
-
             videoAttr: {
               preload: "auto",
               src: videoSrc,
             },
-
             playerIcon : {
               width : '50px',
               height : '50px'
             },
-
             duration: "02:31",
-
             poster: images,
-
             isAdsorb : true,
             autoHeight : true,
             shadowBg : true
 
-          }
-
+          };
           var apl = aPlayer.createPlayer(data);
         }
       });
