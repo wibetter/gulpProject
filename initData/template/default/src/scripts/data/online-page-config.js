96f44bf75fd52060f528110a1b8b2d0e9fda351f
liudan6 <liudan6@staff.sina.com.cn>
2019-11-08 13:08:50 +0800

    调整默认系统模板资源根路径

--- initData/template/default/src/scripts/data/online-page-config.js
+++ initData/template/default/src/scripts/data/online-page-config.js
@@ -6,7 +6,7 @@ var PAGEDATA = {
   page: 0, // 推荐分页：默认取第一页
   page_size: 15, // 推荐分页：默认取一页取15个
   // 项目根地址（静态资源引用根地址）：require-config.js 的baseUrl引用此变量
-  projectBase: '//ssfe.test.sina.com.cn/sports/webSystem/#projectName#/src',
+  projectBase: '//goodtool666.cn/sports/webSystem/#projectName#/src',
   /* 相关推荐的参数 */
   cateid: '2L', // 栏目列表
   cre: 'sptapp', // 这两个参数主要用于描述产品位，需要statics=1激活这两个参数
